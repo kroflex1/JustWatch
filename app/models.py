b2377ff3126905eae4a2e6f2390a092ee452951d
Artyom Sokolov <15012003artem@mail.ru>
2023-04-07 12:03:23 +0500

    добавил модель видео

--- app/models.py
+++ app/models.py
@@ -12,3 +12,13 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    video = relationship("Video", back_populates="video")
+
+
+class Video(Base):
+    __tablename__ = "videos"
+
+    video_id = Column(Integer, primary_key=True, index=True)
+    video_name = Column(String, index=True)
+    author_id = Column(Integer, ForeignKey("users.id"))
+    author = relationship("User", back_populates="author")
