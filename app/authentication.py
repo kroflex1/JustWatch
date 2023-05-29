62d3118f78735bfafadb3e9ee880f7543734980a
Artyom Sokolov <15012003artem@mail.ru>
2023-05-29 23:37:21 +0500

    some changes

--- app/authentication.py
+++ app/authentication.py
@@ -1,19 +1,21 @@
+import os
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from . import errors, schemas
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+
 
 
 class TokenManager:
+    __SECRET_KEY = os.getenv('SECRET_KEY')
+    __ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 15
     REFRESH_TOKEN_EXPIRE_DAYS = 60
 
     @staticmethod
     def create_token(user_id: int, expire: timedelta):
         to_encode = {"user_id": user_id, "exp": datetime.utcnow() + expire}
-        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        encoded_jwt = jwt.encode(to_encode, os.getenv('SECRET_KEY'), algorithm=TokenManager.__ALGORITHM)
         return encoded_jwt
 
     @staticmethod
@@ -25,7 +27,7 @@ class TokenManager:
     @staticmethod
     def try_get_user_id_from_token(token: str) -> int:
         try:
-            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=[TokenManager.__ALGORITHM])
             if datetime.fromtimestamp(int(payload.get("exp"))) < datetime.now():
                 raise errors.TokenExpireError
             user_id: str = payload.get("user_id")
